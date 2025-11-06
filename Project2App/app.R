e2b66421051b0a25586f8eeacd32d504888af22f
“Bailey <balliso2@ncsu.comgit config --global --listgit config --global user.name Bailey Allisongit config --global user.email balliso2@ncsu.com>
2025-11-06 11:03:54 -0500

    start of app

---
+++ Project2App/app.R
@@ -0,0 +1,76 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(ggplot2)
+
+# Read in Seoul Bike Data
+bike_data <- read.csv("SeoulBikeData.csv", header = TRUE, fileEncoding = "latin1")
+# Tidy up column names
+colnames(bike_data) <- (c("Date", "Rented_Bike_Count", "Hour", "Temperature_C", "Humidity_%", "Wind_speed_m/s", "Visibility_10m", "Dew_point_temp_C", "Solar_Radiation_MJ/m2", "Rainfall_mm", "Snowfall_cm", "Seasons", "Holiday", "Functioning_Day"))
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Seoul Bike Data Explorer"),
+
+    # Sidebar 
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("season", "Select Season",
+                    choices = unique(bike_data$Seasons),
+                    selected = unique(bike_data$Seasons),
+                    multiple = TRUE),
+        
+        selectInput("holiday", "Select Holiday Type",
+                    choices = unique(bike_data$Holiday),
+                    selected = unique(bike_data$Holiday),
+                    multiple = TRUE),
+        
+        selectInput("holiday", "Select Numeric Variable",
+                    choices = unique(bike_data$Holiday),
+                    selected = unique(bike_data$Holiday),
+                    multiple = TRUE),
+        
+        sliderInput("rainfall", "Select Rainfall (mm)",
+                    min = min(bike_data$Rainfall_mm),
+                    max = max(bike_data$Rainfall_mm),
+                    value = c(min(bike_data$Rainfall_mm), max(bike_data$Rainfall_mm))),
+        
+        sliderInput("temperature", "Select Temperature (°C)",
+                    min = min(bike_data$Temperature_C),
+                    max = max(bike_data$Temperature_C),
+                    value = c(min(bike_data$Temperature_C), max(bike_data$Temperature_C)))
+      ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)

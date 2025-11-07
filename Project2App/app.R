b600c87357f070765f73b0765892b23076207316
“Bailey <balliso2@ncsu.comgit config --global --listgit config --global user.name Bailey Allisongit config --global user.email balliso2@ncsu.com>
2025-11-06 21:34:46 -0500

    edit about tab

--- Project2App/app.R
+++ Project2App/app.R
@@ -8,11 +8,11 @@ bike_data <- read.csv("SeoulBikeData.csv", header = TRUE, fileEncoding = "latin1
 # Tidy up column names
 colnames(bike_data) <- (c("Date", "Rented_Bike_Count", "Hour", "Temperature_C", "Humidity_%", "Wind_speed_m/s", "Visibility_10m", "Dew_point_temp_C", "Solar_Radiation_MJ/m2", "Rainfall_mm", "Snowfall_cm", "Seasons", "Holiday", "Functioning_Day"))
 
-# Define UI for application that draws a histogram
+# UI
 ui <- fluidPage(
 
     # App title
-    titlePanel("Seoul Bike Data Explorer"),
+    titlePanel("Seoul Bike Sharing Data Explorer"),
 
     # Sidebar 
     sidebarLayout(
@@ -34,31 +34,41 @@ ui <- fluidPage(
         mainPanel(
           tabsetPanel(
             tabPanel("About", 
-                     img(src = "datasetcover.jpg", height = "100%", width = "100%"),
-                     h1("About Seoul Bike Data Explorer"), 
-                     h3("Purpose of App"), 
-                     p("the purpose of this app is to..."),
+                     img(src = "seoulbike3.png", height = "100%", width = "100%"),
+                     h1("About Seoul Bike Sharing Data Explorer"), 
+                     h2("Purpose of this App"), 
+                     p("The purpose of this app is to view data from the Seoul Bike Sharing datasheet, to discover relationships in the data. "),
+                     br(),
+                     p("The tabs along the top of the app can be clicked, showing the following:"),
                      tags$ul(
-                       tags$li("do this thing"),
-                       tags$li("do this thing")
+                       tags$li("The 'About' section describes the app and how to use it, and gives some background on the dataset."),
+                       tags$li("The 'Data Download' section allows the user to view parts of the datasheet based on inputs from the sidebar, and download them to a computer. "),
+                       tags$li("The 'Data Exploration' tab allows the user to obtain numeric and graphical summaries from the data, based on inputs on the sidebar.")
                      ),
-                     h3("Purpose of sidebar and tabs"), 
-                     p("these tabs do this..."),
+                     br(),
+                     p("The sidebar along the lefthand edge of the app is where a user can select numeric and categorical variables to view in the dataset. A user can:"),
                      tags$ul(
-                       tags$li("do this thing"),
-                       tags$li("do this thing")
-                     ),
-                     h3("Dataset"),
-                     img(src = "kagglelogo.png", height = "25%", width = "25%"),
-                     p("this data is about..."),
-                     tags$a(href="https://www.kaggle.com/datasets/saurabhshahane/seoul-bike-sharing-demand-prediction", "Click here for more information!")),
-            tabPanel("Data Table", 
+                       tags$li("Select up to four different seasons to view data from."),
+                       tags$li("Select to view data from holidays vs nonholidays."),
+                       tags$li("Choose to view one of two numeric variables: temperature or rainfall."),
+                       tags$li("Use the slider to determine a range of temperature or rainfall to view."),
+                       br(),
+                       img(src = "seoulbike2.png", height = "100%", width = "100%")),
+                     h2("About the data"),
+                     p("The Seoul Bike Sharing dataset contains counts of public bicycles rented per hour in the Seoul Bike Sharing System. Corresponding weather and holiday information was also recorded. The dataset can be used to predict how atmospheric conditions affect the number of bikes rented."),
+                     p("Please click the link below for more information."),
+                     br(),
+                     tags$a(href="https://www.kaggle.com/datasets/saurabhshahane/seoul-bike-sharing-demand-prediction", "Click here for more information!"),
+                     br(), br(), br(),
+                     img(src = "seoulbike.jpeg", height = "100%", width = "100%")),
+            tabPanel("Data Table", br(), br(),
                      downloadButton(outputId = "downloadData", 
                                     label = "Click here to download table!", 
                                     class = NULL),
+                     br(), br(), br(),
                      DT::dataTableOutput("filteredTable"))
             ,
-            tabPanel("Data Exploration", h3("This is the Plots tab")))
+            tabPanel("Data Exploration", h3("This is the plots tab")))
         )
     )
 )
